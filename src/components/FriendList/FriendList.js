df28f6d234e86d9353efde3f4b6c54be1a06fe48
IgnatevD <ignatevds@gmail.com>
2021-08-18 14:19:48 +0300

    Дз 1 реакт

---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,15 @@
+const FriendList = ({ friends }) => {
+  return (
+    <ul className="friend-list">
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <li className="item" key={id}>
+          <span className="status">{isOnline}</span>
+          <img className="avatar" src={avatar} alt={name} width="48" />
+          <p className="name">{name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default FriendList;
